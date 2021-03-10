1ef506fa4156e2f24df2ad1d3cdd72d6ebf46b86
Youssef SEDDIKI <yseddiki@helb-prigogine.be>
2021-03-10 08:36:20 +0100

    Ajoutez des fichiers projet.

---
+++ MeteoStation_2/Class/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoStation_2.Class
+{
+    class Base
+    {
+        public int id { get; set; }
+        public int cptOctet { get; set; }
+        public int Type { get; set; }
+        public int checksum { get; set; }
+        public UInt32 data{ get; set; }
+    }
+}
